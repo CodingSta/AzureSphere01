0dc4adf50a53081c4ade866b546cd487a11f5201
mingyu, ju <knighp@gmail.com>
2021-01-13 17:50:14 +0900

    2021 0113

--- Samples/HelloWorld/HelloWorld_HighLevelApp/main.c
+++ Samples/HelloWorld/HelloWorld_HighLevelApp/main.c
@@ -46,19 +46,31 @@ int main(void)
 {
     Log_Debug("Starting CMake Hello World application...\n");
 
-    int fd = GPIO_OpenAsOutput(SAMPLE_LED, GPIO_OutputMode_PushPull, GPIO_Value_High);
-    if (fd == -1) {
-        Log_Debug(
-            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
-            strerror(errno), errno);
+    int fd0 = GPIO_OpenAsOutput(MT3620_GPIO5, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    int fd1 = GPIO_OpenAsOutput(MT3620_GPIO6, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    int fd2 = GPIO_OpenAsOutput(MT3620_GPIO7, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    int fd3 = GPIO_OpenAsOutput(MT3620_GPIO8, GPIO_OutputMode_PushPull, GPIO_Value_High);
+
+    if (fd0 < 0) {
+        Log_Debug("Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n", strerror(errno), errno);
         return ExitCode_Main_Led;
     }
 
     const struct timespec sleepTime = {.tv_sec = 1, .tv_nsec = 0};
     while (true) {
-        GPIO_SetValue(fd, GPIO_Value_Low);
+        //GPIO_SetValue(fd0, GPIO_Value_Low);
+        GPIO_SetValue(fd1, GPIO_Value_Low);
         nanosleep(&sleepTime, NULL);
-        GPIO_SetValue(fd, GPIO_Value_High);
+        GPIO_SetValue(fd2, GPIO_Value_Low);
+        nanosleep(&sleepTime, NULL);
+        GPIO_SetValue(fd3, GPIO_Value_Low);
+        nanosleep(&sleepTime, NULL);
+
+        GPIO_SetValue(fd1, GPIO_Value_High);
+        nanosleep(&sleepTime, NULL);
+        GPIO_SetValue(fd2, GPIO_Value_High);
+        nanosleep(&sleepTime, NULL);
+        GPIO_SetValue(fd3, GPIO_Value_High);
         nanosleep(&sleepTime, NULL);
     }
 }
